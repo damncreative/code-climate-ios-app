f4508eba8d87fcf41e8483cfbf6795ce402ea2a1
Mohammed Neundorf <mne@damncreative.de>
2014-06-27 01:16:25 +0200

    added

---
+++ codeclimate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  codeclimate
+//
+//  Created by Nax on 27.06.14.
+//  Copyright (c) 2014 damn creative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
